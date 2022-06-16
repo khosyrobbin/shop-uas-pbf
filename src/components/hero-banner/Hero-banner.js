fe6238805c32d87890b68f3ea12528d499e0d7ae
Zwarzen <akurisang12@gmail.com>
2022-06-16 15:51:10 +0700

    beri navigasi di dashboard

--- src/components/hero-banner/Hero-banner.js
+++ src/components/hero-banner/Hero-banner.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Hero-banner.css';
 import Navigation from '../navigation/Navigation';
+import { Link } from 'react-router-dom';
 import firebaseConfig from "../../config";
 
 const HeroBanner = () => {
 
     return (
         <div className="hero-banner row">
+            <div className="col-8 items">
+                <ul>
+                    <li><Link to={`/dashboard`}>HOME</Link></li>
+                    <li><Link to={`/shoplist`}>SHOP</Link></li>
+                    <li><Link to={`/cart`}>CART</Link></li>
+                    <li><Link to={`/profiles`}>PROFILE</Link></li>
+                    <li><Link onClick={() => firebaseConfig.auth().signOut()}>SIGN OUT</Link></li>
+                </ul>
+            </div>
+            
             <div className="text col-6 ">
                 <h1>Neo OL Shop</h1>
-                <p>Find what you need easely in our shop!   </p>
-                <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
+                <p>Find what you need easely in our shop!</p>
             </div>
         </div>
 
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -78,4 +78,4 @@ const mapStoreToProps = (store) => {
     }
 };
 
-export default withRouter(connect(mapStoreToProps)(Navigation));
+// export default withRouter(connect(mapStoreToProps)(Navigation));
